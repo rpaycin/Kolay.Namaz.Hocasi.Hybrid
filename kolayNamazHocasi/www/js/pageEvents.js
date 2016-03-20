f3a62f9b93e6b8377c000bea1d0496f9bed10033
Reşit PAYÇIN <rpaycin@gmail.com>
2016-03-20 16:13:13 +0200

    1

--- kolayNamazHocasi/www/js/pageEvents.js
+++ kolayNamazHocasi/www/js/pageEvents.js
@@ -18,17 +18,3 @@ $(document).on("pageinit", function () {
     jQuery.support.cors = true;
     $.mobile.allowCrossDomainPages = true;
 });
-
-$(document).on("pagecontainershow", function () {
-    ScaleContentToDevice();
-
-    $(window).on("resize orientationchange", function () {
-        ScaleContentToDevice();
-    })
-});
-
-function ScaleContentToDevice() {
-    scroll(0, 0);
-    var content = $.mobile.getScreenHeight() - $(".ui-header").outerHeight() - $(".ui-footer").outerHeight() - $(".ui-content").outerHeight() + $(".ui-content").height();
-    $(".ui-content").height(content);
-}
