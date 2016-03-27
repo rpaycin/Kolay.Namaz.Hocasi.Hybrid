63da03d736280d1b46064bfaf94420c78df21811
Reşit PAYÇIN <rpaycin@gmail.com>
2016-03-27 23:30:52 +0300

    1

---
+++ kolayNamazHocasi/www/js/startup.js
@@ -0,0 +1,17 @@
+﻿$(document).on("pagebeforecreate", function (event) {
+    $.get("../views/startupHeader.html", function (data) {
+        $(data).insertAfter("head");
+    });
+
+    $.get("../views/startupLeftMenuAbdest.html", function (data) {
+        $("#navLeftMenuAbdest").append(data);
+    });
+
+    $.get("../views/startupLeftMenuNamaz.html", function (data) {
+        $("#navLeftMenuNamaz").append(data);
+    });
+
+    $.get("../views/startupLeftMenuDualar.html", function (data) {
+        $("#navLeftMenuDualar").append(data);
+    });
+});
